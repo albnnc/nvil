a7c97e4890ed3d957718ed19c01f2f19961de48c
albnnc <albnnc@gmail.com>
2023-05-29 12:57:22 +0300

    feat(atoms/storybook): ability to set base href

--- atoms/storybook/ui/server.ts
+++ atoms/storybook/ui/server.ts
@@ -4,10 +4,15 @@ import { handleStoryReloadRequest } from "../story_reload.ts";
 const currentDir = path.fromFileUrl(import.meta.resolve("./"));
 const metaGlob = path.join(currentDir, "./stories/*/meta.json");
 const indexHtmlPath = path.join(currentDir, "./index.html");
+const baseHref = Deno.env.get("BASE_HREF") || "/";
+const indexHtml = await Deno.readTextFile(indexHtmlPath).then((v) =>
+  v.replace(`<base href="/" />`, `<base href="${baseHref}" />`)
+);
 
 server.serve(
   async (req) => {
     const { pathname } = new URL(req.url);
+    const filePath = path.join(currentDir, pathname);
     if (pathname === "/stories") {
       const metas: unknown[] = [];
       for await (const v of fs.expandGlob(metaGlob)) {
@@ -15,16 +20,22 @@ server.serve(
       }
       return new Response(JSON.stringify(metas));
     }
+    if (
+      pathname === "index.html" ||
+      (await Deno.lstat(filePath)
+        .then(() => false)
+        .catch(() => true))
+    ) {
+      return new Response(indexHtml, {
+        headers: { "content-type": "text/html; charset=UTF-8" },
+      });
+    }
     return (
       handleStoryReloadRequest(req) ||
-      fileServer
-        .serveDir(req, {
-          fsRoot: currentDir,
-          quiet: true,
-        })
-        .then((v) =>
-          v.status === 404 ? fileServer.serveFile(req, indexHtmlPath) : v
-        )
+      fileServer.serveDir(req, {
+        fsRoot: currentDir,
+        quiet: true,
+      })
     );
   },
   {
