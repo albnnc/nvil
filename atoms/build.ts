140925327cdac71971752b2f020775002280b09f
Alexander Emelyanov <alexndr.emelyanov@gmail.com>
2023-11-23 18:39:31 +0300

    fix(atoms/build): re-throw build errors if needed (#1)

--- atoms/build.ts
+++ atoms/build.ts
@@ -63,7 +63,11 @@ export function build(
         bundle.set(metaUrl, { data: encoder.encode(JSON.stringify(metafile)) });
         await runStage("BUILD_END", absoluteEntryPoint);
       } catch (e) {
-        logger.error(e.message);
+        if (dev) {
+          logger.error(e.message);
+        } else {
+          throw e;
+        }
       }
     };
     const watch = async () => {
